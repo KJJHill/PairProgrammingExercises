531f66d4130b515debfd1bac8f27983a353d41c4
Kendallene Hill <kendallene@gmail.com>
2017-02-01 15:01:09 -0500

    Initial Part of W3D3 Pair Exercises

---
+++ m1-w3d3-polymorphism-exercise-pair/EmployeePayroll/Classes/SalaryWorker.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayroll.Classes
+{
+    public class SalaryWorker : IWorker
+    {
+
+        private string lastName;
+
+        private string firstName;
+
+        private double annualSalary;
+
+        public double AnnualSalary
+        {
+            get { return annualSalary; }
+            set { annualSalary = value; }
+        }
+        
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+  
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public SalaryWorker(double annualSalary, string firstName, string lastName)
+        {
+            
+        }
+        public double CalculateWeeklyPay(int hoursWorked)
+        {
+            double result = 0.0;
+
+            return result;
+        }
+
+
+
+    }
+}
